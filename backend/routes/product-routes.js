380a979dbbac3cf28e6cbc9cd75a86ac2177761b
Nick Zimmermann <nickzimmermann@Nicks-MacBook-Pro-2.local>
2021-08-23 21:29:24 +0400

    update product routes for delete and update

--- backend/routes/product-routes.js
+++ backend/routes/product-routes.js
@@ -23,4 +23,65 @@ router.post('/create',
         );
     }
 );
+
+// the following code is just a template and needs to be changed for the product (package, description, price)
+// http://www.myapp.com/product/update
+router.post('/update',
+    (req, res) => {
+
+        ProductModel
+        .findOneAndUpdate(
+            { 'model': 'iPhone 12' },
+            {
+                $set: {
+                   'price': 3800 
+                }
+            }
+        )
+         // If the item is found in the database...
+         .then(
+            (dbDocument) => {
+                res.send(dbDocument);
+            }
+        )
+        // If the item is NOT found in the database...
+        .catch(
+            (error) => {
+                console.log('mongoose error', error);
+            }
+        );
+
+    }
+);
+
+// the following code is just a template and is not working yet. Delete has to be implemented.
+// http://www.myapp.com/product/update
+router.post('/delete',
+    (req, res) => {
+
+        ProductModel
+        .findOneAndUpdate(
+            { 'model': 'iPhone 12' },
+            {
+                $set: {
+                   'price': 3800 
+                }
+            }
+        )
+         // If the item is found in the database...
+         .then(
+            (dbDocument) => {
+                res.send(dbDocument);
+            }
+        )
+        // If the item is NOT found in the database...
+        .catch(
+            (error) => {
+                console.log('mongoose error', error);
+            }
+        );
+
+    }
+);
+
 module.exports = router;
